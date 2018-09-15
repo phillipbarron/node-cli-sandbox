2326a495353fa67fdfaf1526cca274d824eeb2c8
Phill Barron <phillipbarron242@gmail.com>
2018-09-15 16:37:14 +0100

    added check for values already set as env props. this are filtered where already set

--- src/index.js
+++ src/index.js
@@ -11,16 +11,39 @@ const requiredProperties = [
     }
 ];
 
+const filterOutExistingPropeties = () => {
+    const bees =  requiredProperties.reduce((init, current) => {
+        if(!process.env[current.name]) {
+            init.push(current);
+            return init;
+        }
+    }, []);
+    return bees;
+}
+
+const getRequiredProps = () => {
+    return requiredProperties.reduce((init, current) => {init.push(process.env[current.name]); return init}, []);
+};
+
 new Promise((resolve, reject) => {
+    const missingProperties = filterOutExistingPropeties();
+    if (!missingProperties) {
+        console.log('there are no missing properties');
+        resolve();
+    };
     prompt.start();
-    prompt.get(requiredProperties, (err, result) => {
+
+    prompt.get(missingProperties, (err, result) => {
         if(err) {
             console.log('it done a wrong', err);
             reject(1);
         }
+        for(var key in result) {
+            process.env[key] = result[key];
+        }
         resolve(result);
     });
-}).then(userDetails => {
-    console.log('all the details are', userDetails);
-    console.log('I do not want this to execute till after input');    
-});
+}).then(result => {
+    console.log('all the details are', getRequiredProps(), result);
+    }
+);
