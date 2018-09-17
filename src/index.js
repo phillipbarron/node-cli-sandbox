7f0c7d1af7e72d733ac5f07b451575e525fdb8ff
Phill Barron <phillipbarron242@gmail.com>
2018-09-17 19:55:58 +0100

    renaming for clarity

--- src/index.js
+++ src/index.js
@@ -1,53 +1,57 @@
-const prompt = require('prompt');
-const colours = require('colors');
+const prompt = require("prompt");
+const colours = require("colors");
 
 prompt.message = "Please enter required value".green;
 
-const requiredProperties = [
-    {
-        name: 'E2E_TEST_USERNAME'
-    },
-    {
-        name: 'E2E_TEST_USERNAME_PASSWORD',
-        hidden: true,
-        replace: '*'
-    }
+const requiredValues = [
+  {
+    name: "E2E_TEST_USERNAME"
+  },
+  {
+    name: "E2E_TEST_USERNAME_PASSWORD",
+    hidden: true,
+    replace: "*"
+  }
 ];
 
 const filterOutExistingPropeties = () => {
-    return requiredProperties.reduce((init, current) => {
-        if(!process.env[current.name]) {
-            init.push(current);
-            return init;
-        }
-        return init;
-    }, []);
-}
+  return requiredValues.reduce((init, current) => {
+    if (!process.env[current.name]) {
+      init.push(current);
+      return init;
+    }
+    return init;
+  }, []);
+};
 
-const getRequiredPropsFromEnvironment = () => {
-    return requiredProperties.reduce((init, current) => {init.push(process.env[current.name]); return init}, []);
+const getRequiredValuesFromEnvironment = () => {
+  return requiredValues.reduce((init, current) => {
+    init.push(process.env[current.name]);
+    return init;
+  }, []);
 };
 
 new Promise((resolve, reject) => {
-    const missingProperties = filterOutExistingPropeties();
-    if (!missingProperties) {
-        console.log('there are no missing properties');
-        resolve();
-    };
-    prompt.start();
+  const missingValues = filterOutExistingPropeties();
+  if (!missingValues) {
+    console.log("there are no missing properties");
+    resolve();
+  }
+  prompt.start();
 
-    prompt.get(missingProperties, (err, result) => {
-        if(err) {
-            console.log('failed to get required input', err);
-            reject(1);
-        }
-        for(var key in result) {
-            process.env[key] = result[key];
-        }
-        resolve(getRequiredPropsFromEnvironment());
-    });
-}).then(result => {
-    console.log('all the details are', result);
-    console.log('at this stage, we have everything that we need so we can carry on');
+  prompt.get(missingValues, (err, result) => {
+    if (err) {
+      console.log("failed to get required input", err);
+      reject(1);
     }
-);
+    for (var key in result) {
+      process.env[key] = result[key];
+    }
+    resolve(getRequiredValuesFromEnvironment());
+  });
+}).then(result => {
+  console.log("all the details are", result);
+  console.log(
+    "at this stage, we have everything that we need so we can carry on"
+  );
+});
